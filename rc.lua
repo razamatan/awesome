484cfa39f300a8a0fd6bab0053a68b0ba698744c
razamatan <razamatan@hotmail.com>
2013-11-16 16:46:36 -0800

    calendar widget

--- rc.lua
+++ rc.lua
@@ -54,7 +54,6 @@ browser = "google-chrome-stable"
 -- Themes define colours, icons, and wallpapers
 beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
-
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
@@ -147,7 +146,39 @@ local separator = wibox.widget.textbox()
 separator:set_text(" ◦ ")
 
 -- Create a textclock widget
+local calendar = nil
+local offset = 0
 mytextclock = awful.widget.textclock(" %a %m/%d %H:%M ", 60)
+
+function remove_calendar()
+    if calendar ~= nil then
+        naughty.destroy(calendar)
+        calendar = nil
+        offset = 0
+    end
+end
+
+function add_calendar(inc_offset)
+    local save_offset = offset
+    remove_calendar()
+    offset = save_offset + inc_offset
+    local datespec = os.date("*t")
+    datespec = datespec.year * 12 + datespec.month - 1 + offset
+    datespec = (datespec % 12 + 1) .. " " .. math.floor(datespec / 12)
+    local cal = awful.util.pread("cal -m " .. datespec)
+    cal = string.gsub(cal, "^%s*(.-)%s*$", "%1")
+    calendar = naughty.notify({
+        text = string.format('<span font_desc="terminus">%s</span>', cal),
+        timeout = 0,
+        hover_timeout = 0.5,
+    })
+end
+
+mytextclock:connect_signal("mouse::enter", function() add_calendar(0) end)
+mytextclock:connect_signal("mouse::leave", remove_calendar)
+mytextclock:buttons( awful.util.table.join(
+    awful.button({ }, 4, function() add_calendar(-1) end),
+    awful.button({ }, 5, function() add_calendar(1)  end)))
 
 -- Bashets
 local bashets_shm_path = string.format("/run/tmp/%s-bashets/", os.getenv("USER"))
