cbf64362a4c7f468ae90ba98fa40b1340e3a2ccf
razamatan <razamatan@hotmail.com>
2017-03-22 00:42:46 -0700

    remove old bashets

--- mybashets/cpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-id=$1
-id=${id:=[0-9]*}
-for i in /sys/devices/system/cpu/cpu$id/cpufreq ; do
-   max=`sudo cat $i/cpuinfo_max_freq`
-   cur=`sudo cat $i/cpuinfo_cur_freq`
-   per=$(($cur*100/$max))
-   echo $max $cur $per
-done
--- mybashets/vollevel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-
-RES=$(amixer get Master | grep "Front Left:" | awk '{ if ($6 == "[off]") {print "[0%]"} else {print $5}}')
-#echo -e $RES
-#RES="[30%]"
-RES=${RES##"["}
-RES=${RES%%"%]"}
-echo -n $RES #> /tmp/vollevel.out
